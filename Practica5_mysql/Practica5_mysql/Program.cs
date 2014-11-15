1c7fc234c7040f30d595e0cdd0ef438964046e29
damaris Baldenegro <damarisbaldenegro@gmail.com>
2014-11-14 23:19:11 -0600

    Version 1.0

---
+++ Practica5_mysql/Practica5_mysql/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+
+namespace Practica5_mysql
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Alumnos al = new Alumnos();
+			int registros= 0;
+			
+				
+		
+			
+				Console.WriteLine("Elige ");
+				Console.WriteLine("1.Agrega tupla");
+				Console.WriteLine("2.Consultar tuplas.");
+				
+				
+				int con = Int32.Parse( Console.ReadLine() );
+				switch(con)
+						
+					{
+					case 1:
+						
+						int codigo = 0;
+						Console.WriteLine("dame el codigo del Alumno:");
+						codigo = int.Parse( Console.ReadLine() );
+						
+								
+						Console.WriteLine("dame el nombre del Alumno:");
+						String nombre = Console.ReadLine();
+						int telefono = 0;
+					
+						Console.WriteLine("Ingrese el TELÉFONO del Alumno:");
+						telefono = Int.Parse( Console.ReadLine() );
+					
+						
+						Console.WriteLine("Ingrese el E-MAIL del Alumno:");
+						String email = Console.ReadLine();
+					
+										
+						al.insertarRegistroNuevo(codigo,nombre,telefono,email);
+						break;	
+						
+						
+					case 2:
+			
+					al.mostrarTodos();
+				
+						break;
+					
+						
+					
+				}
+				
+			Console.ReadKey(true);	
+		}
+	}
+}
+
+
---
+++ Practica5_mysql/Practica5_mysql/connectionmysql.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Damaris Baldenegro
+ * Date: 14/11/2014
+ * Time: 11:14 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Practica5_mysql
+{
+	/// <summary>
+	/// Description of connectionmysql.
+	/// </summary>
+	public class connectionmysql
+	{
+		public connectionmysql()
+		{
+		}
+	}
+}
